d72bee8bcf6ab95870a1834a3364e8f3fd42d1fc
Mike Jovanovich <mpjovanovich@gmail.com>
2024-07-14 19:56:38 -0400

    Pre-deploy commit

--- src/compile.ts
+++ src/compile.ts
@@ -79,13 +79,13 @@ function getSiteHtml(html: string, frontmatter?: Frontmatter): string {
             rel="icon"
             type="image/png"
             sizes="32x32"
-            href="assets/images/favicon-32x32.png"
+            href="/assets/images/favicon-32x32.png"
         >
         <link
             rel="icon"
             type="image/png"
             sizes="16x16"
-            href="assets/images/favicon-16x16.png"
+            href="/assets/images/favicon-16x16.png"
         >
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
